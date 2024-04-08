fa23c45a4b21625f09319445b3bbb48d69d5255e
Jhonatec <jhonsreis@gmail.com>
2024-04-08 14:41:53 -0300

    feat: tooltip with lib and tailwind config

---
+++ frontend/src/components/Header/links.ts
@@ -0,0 +1,26 @@
+export const links = [
+  {
+    name: 'GitHub',
+    url: 'https://github.com/jhonatec-dev'
+  },
+  {
+    name: 'HackerRank',
+    url: 'https://www.hackerrank.com/jhonatec'
+  },
+  {
+    name: 'LinkedIn',
+    url: 'https://www.linkedin.com/in/jhonatec/'
+  },
+  {
+    name: 'YouTube',
+    url: 'https://www.youtube.com/c/Jhonatec'
+  },
+  {
+    name: 'Instagram',
+    url: 'https://www.instagram.com/_jhonatec/'
+  },
+  {
+    name: 'TikTok',
+    url: 'https://www.tiktok.com/@jhonatec'
+  }
+]
