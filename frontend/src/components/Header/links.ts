9e5a06d9b3e829a5d31b9e2956974c5e59e9d320
Jhonatec <jhonsreis@gmail.com>
2024-04-09 17:44:57 -0300

    feat: SEO improvements

--- frontend/src/components/Header/links.ts
+++ frontend/src/components/Header/links.ts
@@ -1,18 +1,33 @@
 export const links = [
   {
-    name: 'GitHub',
-    url: 'https://github.com/jhonatec-dev'
+    name: "GitHub",
+    url: "https://github.com/jhonatec-dev",
   },
   {
-    name: 'HackerRank',
-    url: 'https://www.hackerrank.com/profile/jhonatec'
+    name: "HackerRank",
+    url: "https://www.hackerrank.com/profile/jhonatec",
   },
   {
-    name: 'LinkedIn',
-    url: 'https://www.linkedin.com/in/jhonatec/'
+    name: "LinkedIn",
+    url: "https://www.linkedin.com/in/jhonatec/",
   },
   {
-    name: 'YouTube',
-    url: 'https://www.youtube.com/c/Jhonatec'
+    name: "YouTube",
+    url: "https://www.youtube.com/c/Jhonatec",
   },
-]
+];
+
+class Reaction {
+  name: string;
+  constructor(algo: any) {
+    this.name = "Reaction";
+  }
+}
+
+function seePortfolio() {
+  const URL = "https://jhonatec.dev";
+  open(URL, "_blank");
+  return new Reaction("awesome!");
+}
+
+seePortfolio();
