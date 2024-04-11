e7055175d159ed93c8fa055943c234d7b428116e
Jhonatec <jhonsreis@gmail.com>
2024-04-10 22:57:00 -0300

    feat: certifications working

--- frontend/src/components/Header/Certifications/certs.ts
+++ frontend/src/components/Header/Certifications/certs.ts
@@ -8,11 +8,11 @@ export const certs = [
     url: "https://cert.efset.org/TkPafK",
   },
   {
-    title: "English Proficiency",
-    description: "CEFR Level C2",
-    issuer: "EF SET",
-    image: "/assets/images/certs/ef-cefr.png",
-    logo: "/assets/images/certs/ef-logo.png",
-    url: "https://cert.efset.org/TkPafK",
+    title: "Python INTRO",
+    description: "Curso de Python para principiantes",
+    issuer: "FIAP",
+    image: "/assets/images/certs/fiap-python.jpg",
+    logo: "/assets/images/certs/fiap-logo.png",
+    url: "https://on.fiap.com.br/pluginfile.php/1/local_nanocourses/certificado_nanocourse/105724/2caef4566e5080406a2796d32448d6f3/certificado.png",
   }
 ];
